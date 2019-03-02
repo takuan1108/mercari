5033b2174828916335de3c10957ffe929eac1c80
奥村光貴 <>
2019-03-03 00:28:46 +0900

    buy view create first commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,6 @@ Rails.application.routes.draw do
     end
   end
   resources :items
-
+  get 'items/:id/buy' => 'items#buy'
   root 'items#index'
-<<<<<<< HEAD
-
-=======
->>>>>>> ma-kun92/master
 end
