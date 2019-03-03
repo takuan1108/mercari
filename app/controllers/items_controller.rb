5da30344ca1517c80e66ba80b60c48e677a2f738
奥村光貴 <>
2019-03-03 14:42:24 +0900

    pull commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+  end
+
+  def show
+  end
+
+  def buy
+  end
+<<<<<<< HEAD
+=======
+
+>>>>>>> origin/master
 end
