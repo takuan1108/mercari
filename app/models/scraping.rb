7ad06a80fcde97d42e15282f9d61fe5718dd7840
katoShiro46 <work_h_t_m@yahoo.co.jp>
2019-03-04 14:23:12 +0900

    edit_scraping
    スクレイピングうまくできないけど一応内容更新。
    本体の動作には影響ないはずです。

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -5,18 +5,31 @@ class Scraping
     elements = page.search("dl dd a")
       elements.each do |ele|
       prefecture = ele.inner_text
-    prefecture = Prefecture.new(prefecture: prefecture)
-    prefecture.save
+      prefecture = Prefecture.new(prefecture: prefecture)
+      prefecture.save
     end
   end
+
   def self.gets_brands
     agent = Mechanize.new
     page = agent.get("https://www.mercari.com/jp/brand/")
     elements = page.search(".brand-list-initial-box-brand-name div p")
-      elements.each do |ele|
+    elements.each do |ele|
       brand = ele.inner_text
-    brand = Brand.new(brand: brand)
-    brand.save
+      brand = Brand.new(brand: brand)
+      brand.save
+    end
+  end
+
+  def self.gets_categories
+    agent = Mechanize.new
+    current_page = agent.get("https://www.mercari.com/jp/")
+    elements = current_page.search('.pc-header-nav-grand-child')
+    elements.each do |ele|
+      small_category = ele.children.inner_html
+      middle_category = ele.parent.previous_sibling.previous_sibling.inner_html
+      large_category = ele.parent.parent.parent.previous_sibling.previous_sibling.children.inner_html
+      category = Category.new(large: large_category,middle: middle_category,small: small_category)
     end
   end
 end
