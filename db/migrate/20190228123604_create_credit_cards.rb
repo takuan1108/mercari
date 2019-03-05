0b2c07c712acb0db6b09c0e57f5123ccebf179d2
奥村光貴 <>
2019-03-05 15:51:07 +0900

    db edit first commit

--- db/migrate/20190228123604_create_credit_cards.rb
+++ db/migrate/20190228123604_create_credit_cards.rb
@@ -1,7 +1,7 @@
 class CreateCreditCards < ActiveRecord::Migration[5.0]
   def change
     create_table :credit_cards do |t|
-      t.integer :card_number,null: false
+      t.string :card_number,null: false
       t.integer :month,null: false
       t.integer :year,null: false
       t.integer :security_code,null: false
