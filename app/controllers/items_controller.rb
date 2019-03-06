a5600339c7a971f5a7c8a3e0d1a891bad988c5fa
katoShiro46 <work_h_t_m@yahoo.co.jp>
2019-03-06 15:42:15 +0900

    edit_top-viewサーバー
    サーバーから情報を取り出し一覧表示させる段階まで作業
    この後、カテゴリーごとの部分テンプレート化作業を進める

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
 
   def index
+    @items_ladys = Item.includes(:category).where(categories:{large:"レディース"}).limit(4)
+    @items_mens = Item.includes(:category).where(categories:{large:"メンズ"}).limit(4)
+    @items_babys = Item.includes(:category).where(categories:{large:"ベビー・キッズ"}).limit(4)
+    @items_cosmes = Item.includes(:category).where(categories:{large:"コスメ・香水・美容"}).limit(4)
   end
 
   def new
