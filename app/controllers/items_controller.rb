d8228713d0eda04b3de8a294ae51544ca6c2033e
katoShiro46 <work_h_t_m@yahoo.co.jp>
2019-03-08 16:22:16 +0900

    edit_top-view-server
    トップページの新着アイテムをrender+@によりパターン化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,8 @@
 class ItemsController < ApplicationController
 
   def index
-    @items_ladys = Item.includes(:category).where(categories:{large:"レディース"}).limit(4)
-    @items_mens = Item.includes(:category).where(categories:{large:"メンズ"}).limit(4)
-    @items_babys = Item.includes(:category).where(categories:{large:"ベビー・キッズ"}).limit(4)
-    @items_cosmes = Item.includes(:category).where(categories:{large:"コスメ・香水・美容"}).limit(4)
+    @new_items_categories = ["レディース","メンズ","ベビー・キッズ","コスメ・香水・美容"]
+    @new_items_blands = ["シャネル","ルイ ヴィトン","シュプリーム","ナイキ"]
   end
 
   def new
