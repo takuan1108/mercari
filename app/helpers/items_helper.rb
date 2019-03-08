70c084cd8b45c5523ddc6d6220c43ea0145401c0
katoShiro46 <work_h_t_m@yahoo.co.jp>
2019-03-08 21:17:21 +0900

    modify_top-view-server
    Add helper method
    Remove logic form view_file

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,9 @@
 module ItemsHelper
+  def select_items(new_items,name)
+    if new_items.include?(name)
+      @items = Item.includes(:category).where(categories:{large:name}).limit(4)
+    else
+      @items = Item.includes(:brand).where(brands:{brand:name}).limit(4)
+    end
+  end
 end
