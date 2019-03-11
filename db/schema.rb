5fa039fbc2a22d5da9fc896abbfd232caf1d333c
takuan1108 <flying.bed.in.dream@gmail.com>
2019-03-11 20:30:10 +0900

    Add payjp_id column to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190301021300) do
+ActiveRecord::Schema.define(version: 20190307130700) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "brand"
@@ -144,6 +144,7 @@ ActiveRecord::Schema.define(version: 20190301021300) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+    t.string   "payjp_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
