ffb9c3e7ebef49e1dd9a8e55ce505c2e32d15c6f
takuan1108 <flying.bed.in.dream@gmail.com>
2019-03-12 18:52:49 +0900

    Change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :check, if: :devise_controller?
-<<<<<<< HEAD
 
-=======
->>>>>>> 70ce02bc45f42ea873641d2c7ada78f0f12d7ba0
 private
   def after_sign_in_path_for(resource)
     root_path
@@ -16,15 +13,11 @@ private
   end
 
   def configure_permitted_parameters
-<<<<<<< HEAD
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_phonetic, :first_name_phonetic, :birth_year, :birth_month, :birth_day, :payjp_id])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:payjp_id])
-  end
-
-=======
+    devise_parameter_sanitizer.permit(:account_update, keys: [])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_phonetic, :first_name_phonetic, :birth_year, :birth_month, :birth_day])
   end
->>>>>>> 70ce02bc45f42ea873641d2c7ada78f0f12d7ba0
+
   def sign_in_required
       redirect_to new_user_session_url unless user_signed_in?
   end
@@ -32,8 +25,4 @@ private
   # def check
   #   binding.pry
   # end
-<<<<<<< HEAD
-=======
-
->>>>>>> 70ce02bc45f42ea873641d2c7ada78f0f12d7ba0
 end
