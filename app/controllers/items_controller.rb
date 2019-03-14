a10a0e04d13a59b2260032a38d8f976d13338fb4
katoShiro46 <work_h_t_m@yahoo.co.jp>
2019-03-14 12:30:05 +0900

    Add new function (show same_vendor's_and_brand_items)
    Add new function (show same_vendor's_and_brand_items)
    
    essential function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,8 @@ class ItemsController < ApplicationController
     @images = @item.item_images
     @comment = ItemComment.new
     @comments = @item.item_comments
+    @vendor_items = Item.where.not(id:params[:id]).where(vendor_id:@item.vendor_id).order(id: :DESC).limit(6)
+    @brand_items = Item.where.not(id:params[:id]).where(brand:@item.brand).order(id: :DESC).limit(6)
   end
 
   def buy
