e131b28b6137e9cb0efb7394562cd5ba91ba63c1
ma-kun92 <c1nakata@yahoo.co.jp>
2019-03-21 02:26:05 +0900

    edit4
    Edit 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-# Rails.application.config.assets.precompile += %w( *.js )
+
